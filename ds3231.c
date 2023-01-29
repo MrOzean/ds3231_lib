ff062b4f150f2e4ec0c9b2e4bebff9db7a8548d7
Andrey <you@example.com>
2023-01-29 17:29:07 +0700

    chore: comments

--- ds3231.c
+++ ds3231.c
@@ -1,183 +1,257 @@
 #include "ds3231.h"
 
-#define CLOCK_REG_ADDR 0x00 // start address of registers
-#define ALARM1_REG_ADDRESS 0x07
-#define ALARM2_REG_ADDRESS 0x0B
-#define TEMP_REG_ADDR 0x11
-
-
-// convert decimal to BCD (Binary Coded Decimal)
+/*!
+ * @brief convert decimal to BCD (Binary Coded Decimal)
+ */
 static inline uint8_t dec_to_BCD(uint8_t value)
 {
-	return ((value / 10) << 4) | (value % 10);
+    return ((value / 10) << 4) | (value % 10);
 }
-// convert BCD(Binary Coded Decimal) to Decimal
+/*!
+ * @brief convert BCD(Binary Coded Decimal) to Decimal
+ */
 static inline uint8_t BCD_to_dec(uint8_t value)
 {
-	return ((value >> 4) * 10 + (0x0F & value));
+    return ((value >> 4) * 10 + (0x0F & value));
 }
 
-DS3231 *clock_store;
-
-static inline void write_data(uint8_t address, uint8_t *data, uint16_t data_length)
+/*!
+ * @brief Read control register value
+ *
+ * @param[in]  clock         : Pointer to ds3231 device
+ * @param[in]  reg_addr      : Register address (DS3231 has two of it)
+ *
+ * @retval Byte with register value
+ */
+static uint8_t read_control_reg(ds3231_dev *clock, ds3231_control_reg reg_addr)
 {
-	while (HAL_I2C_Mem_Write(
-			   clock_store->i2c_bus,
-			   (uint16_t)clock_store->address,
-			   address,
-			   I2C_MEMADD_SIZE_8BIT,
-			   data,
-			   data_length,
-			   (uint32_t)1000) != HAL_OK)
-	{
-	}
+    uint8_t buff[1];
+
+    clock->read(reg_addr, (uint8_t *)&buff, 1, clock->i2c_chip_ptr);
+
+    return buff[0];
 }
 
-static inline void read_data(uint8_t address, uint8_t *result, uint16_t result_length)
+/*!
+ * @brief Write control register value
+ *
+ * @param[in]  clock          Pointer to ds3231 device
+ * @param[in]  reg_addr       Register address (DS3231 has two of it)
+ * @param[in]  value          New value
+ *
+ * @retval   0   -> Success.
+ * @retval Non zero value -> Fail.
+ */
+static uint8_t write_control_reg(ds3231_dev *clock, ds3231_control_reg reg_addr, uint8_t value)
 {
-	while (HAL_I2C_Mem_Read(
-			   clock_store->i2c_bus,
-			   (uint16_t)clock_store->address,
-			   address,
-			   I2C_MEMADD_SIZE_8BIT,
-			   result,
-			   result_length,
-			   (uint32_t)1000) != HAL_OK)
-	{
-	}
+    uint8_t buff[] = {value};
+
+    int8_t res = clock->write(reg_addr, (uint8_t *)&buff, 1, clock->i2c_chip_ptr);
+
+    return buff[0];
 }
 
-DS3231 ds3231_init(uint8_t address, I2C_HandleTypeDef *i2c_bus)
+/*!
+ * @brief Set time to target clock
+ *
+ * @param[in]  clock         : Pointer to ds3231 device
+ * @param[out] date_time     : Pointer to ds3231_date_time struct in which data to be written
+ *                            is stored.
+ * @retval   0   -> Success.
+ * @retval Non zero value -> Fail.
+ *
+ */
+int8_t ds3231_set_date_time(ds3231_dev *clock, ds3231_date_time *date_time)
 {
-	DS3231 clock;
+    uint8_t buff[7];
 
-	clock.address = address;
-	clock.i2c_bus = i2c_bus;
+    buff[0] = dec_to_BCD(date_time->seconds);
+    buff[1] = dec_to_BCD(date_time->minutes);
+    buff[2] = dec_to_BCD(date_time->hours);
+    buff[3] = dec_to_BCD(date_time->day_of_week);
+    buff[4] = dec_to_BCD(date_time->date);
+    buff[5] = dec_to_BCD(date_time->month);
+    buff[6] = dec_to_BCD(date_time->year - 2000);
 
-	return clock;
+    int8_t res = clock->write(DS3231_CLOCK_REG_ADDR, (uint8_t *)&buff, 7, clock->i2c_chip_ptr);
+
+    return res;
 }
 
-void ds3231_set_date_time(DS3231 *clock, DS3231_DateTime *date_time)
+/*!
+ * @brief Get time from target clock
+ *
+ * @param[in]  clock         : Pointer to ds3231 device
+ * @param[out] date_time     : Pointer to ds3231_date_time struct in which data to be written
+ *                            is stored.
+ * @retval   0   -> Success.
+ * @retval Non zero value -> Fail.
+ *
+ */
+int8_t ds3231_get_date_time(ds3231_dev *clock, ds3231_date_time *date_time)
 {
-	clock_store = clock;
+    uint8_t buff[7];
 
-	uint8_t buff[7];
+    int8_t res = clock->read(DS3231_CLOCK_REG_ADDR, (uint8_t *)&buff, 7, clock->i2c_chip_ptr);
 
-	buff[0] = dec_to_BCD(date_time->seconds);
-	buff[1] = dec_to_BCD(date_time->minutes);
-	buff[2] = dec_to_BCD(date_time->hours);
-	buff[3] = dec_to_BCD(date_time->day_of_week);
-	buff[4] = dec_to_BCD(date_time->date);
-	buff[5] = dec_to_BCD(date_time->month);
-	buff[6] = dec_to_BCD(date_time->year - 2000);
+    date_time->seconds = BCD_to_dec(buff[0]);
+    date_time->minutes = BCD_to_dec(buff[1]);
+    date_time->hours = BCD_to_dec(buff[2]);
+    date_time->day_of_week = BCD_to_dec(buff[3]);
+    date_time->date = BCD_to_dec(buff[4]);
+    date_time->month = BCD_to_dec(buff[5]);
+    date_time->year = BCD_to_dec(buff[6]) + 2000;
 
-	write_data(CLOCK_REG_ADDR, (uint8_t *)&buff, 7);
+    return res;
 }
 
-void ds3231_get_date_time(DS3231 *clock, DS3231_DateTime *date_time)
+/*!
+ * @brief Get time from target clock
+ *
+ * @param[in]  clock         : Pointer to ds3231 device
+ * @param[out] date_time     : Pointer to ds3231_date_time struct in which data to be written
+ *                            is stored.
+ * @retval   0   -> Success.
+ * @retval Non zero value -> Fail.
+ *
+ */
+int8_t ds3231_set_alarm_1(ds3231_dev *clock, ds3231_alarm_1 *alarm)
 {
-	clock_store = clock;
+    uint8_t buff[4]; // buffer to write
 
-	uint8_t buff[7];
+    uint8_t A1M1 = (alarm->alarm_rate & 1) << 7;         // get a1m1 mask
+    uint8_t A1M2 = ((alarm->alarm_rate >> 1) & 1) << 7;  // get a1m2 mask
+    uint8_t A1M3 = ((alarm->alarm_rate >> 2) & 1) << 7;  // get a1m3 mask
+    uint8_t A1M4 = ((alarm->alarm_rate >> 3) & 1) << 7;  // get a1m4 mask
+    uint8_t DY_DT = ((alarm->alarm_rate >> 4) & 1) << 6; // get DY/DT but, 1 if only "Alarm when day, hours, minutes, and seconds match"
 
-	read_data(CLOCK_REG_ADDR, (uint8_t *)&buff, 7);
+    // convert input values to BCD and apply mask
+    buff[0] = dec_to_BCD(alarm->seconds) | A1M1;
+    buff[1] = dec_to_BCD(alarm->minutes) | A1M2;
+    buff[2] = dec_to_BCD(alarm->hours) | A1M3; // valid only for 24h format
+    buff[3] = dec_to_BCD(alarm->day_date) | A1M4 | DY_DT;
 
-	date_time->seconds = BCD_to_dec(buff[0]);
-	date_time->minutes = BCD_to_dec(buff[1]);
-	date_time->hours = BCD_to_dec(buff[2]);
-	date_time->day_of_week = BCD_to_dec(buff[3]);
-	date_time->date = BCD_to_dec(buff[4]);
-	date_time->month = BCD_to_dec(buff[5]);
-	date_time->year = BCD_to_dec(buff[6]) + 2000;
+    int8_t res = clock->write(DS3231_ALARM1_REG_ADDRESS, (uint8_t *)&buff, 4, clock->i2c_chip_ptr);
+
+    return res;
 }
 
-void ds3231_set_alarm_1(DS3231 *clock, DS3231_Alarm_1 *alarm)
+int8_t ds3231_get_alarm_1(ds3231_dev *clock, ds3231_alarm_1 *alarm)
 {
-	clock_store = clock;
+    uint8_t buff[4];
 
-	uint8_t buff[4];
+    alarm->alarm_rate = 0;
 
-	uint8_t a1m1 = (alarm->alarm_rate & 1) << 7;
-	uint8_t a1m2 = ((alarm->alarm_rate >> 1) & 1) << 7;
-	uint8_t a1m3 = ((alarm->alarm_rate >> 2) & 1) << 7;
-	uint8_t a1m4 = ((alarm->alarm_rate >> 3) & 1) << 7;
-	uint8_t dy_dt = ((alarm->alarm_rate >> 4) & 1) << 6;
+    int8_t res = clock->read(DS3231_ALARM1_REG_ADDRESS, (uint8_t *)&buff, 4, clock->i2c_chip_ptr);
 
-	buff[0] = dec_to_BCD(alarm->seconds) | a1m1;
-	buff[1] = dec_to_BCD(alarm->minutes) | a1m2;
-	buff[2] = dec_to_BCD(alarm->hours) | a1m3;
-	buff[3] = dec_to_BCD(alarm->day_date) | a1m4 | dy_dt;
+    uint8_t a1m1 = (buff[0] >> 7) & 1;
+    uint8_t a1m2 = ((buff[1] >> 7) & 1) << 1;
+    uint8_t a1m3 = ((buff[2] >> 7) & 1) << 2;
+    uint8_t a1m4 = ((buff[3] >> 7) & 1) << 3;
+    uint8_t dy_dt = ((buff[3] >> 6) & 1) << 4;
 
-	write_data(ALARM1_REG_ADDRESS, (uint8_t *)&buff, 4);
-	;
+    alarm->alarm_rate = a1m1 | a1m2 | a1m3 | a1m4 | dy_dt;
+
+    alarm->seconds = BCD_to_dec(buff[0] & 0x7F);
+    alarm->minutes = BCD_to_dec(buff[1] & 0x7F);
+    alarm->hours = BCD_to_dec(buff[2] & 0x3F);
+    alarm->day_date = BCD_to_dec(buff[3] & 0x3F);
+
+    return res;
 }
 
-void ds3231_get_alarm_1(DS3231 *clock, DS3231_Alarm_1 *alarm)
+int8_t ds3231_set_alarm_2(ds3231_dev *clock, ds3231_alarm_2 *alarm)
 {
-	clock_store = clock;
+    uint8_t buff[3];
 
-	uint8_t buff[4];
+    uint8_t a2m2 = (alarm->alarm_rate & 1) << 7;
+    uint8_t a2m3 = ((alarm->alarm_rate >> 1) & 1) << 7;
+    uint8_t a2m4 = ((alarm->alarm_rate >> 2) & 1) << 7;
+    uint8_t dy_dt = ((alarm->alarm_rate >> 3) & 1) << 6;
 
-	alarm->alarm_rate = 0;
+    buff[0] = dec_to_BCD(alarm->minutes) | a2m2;
+    buff[1] = dec_to_BCD(alarm->hours) | a2m3;
+    buff[2] = dec_to_BCD(alarm->day_date) | a2m4 | dy_dt;
 
-	read_data(ALARM1_REG_ADDRESS, (uint8_t *)&buff, 4);
+    int8_t res = clock->write(DS3231_ALARM2_REG_ADDRESS, (uint8_t *)&buff, sizeof(buff), clock->i2c_chip_ptr);
 
-	uint8_t a1m1 = (buff[0] >> 7) & 1;
-	uint8_t a1m2 = ((buff[1] >> 7) & 1) << 1;
-	uint8_t a1m3 = ((buff[2] >> 7) & 1) << 2;
-	uint8_t a1m4 = ((buff[3] >> 7) & 1) << 3;
-	uint8_t dy_dt = ((buff[3] >> 6) & 1) << 4;
-
-	alarm->alarm_rate = a1m1 | a1m2 | a1m3 | a1m4 | dy_dt;
-
-	alarm->seconds = BCD_to_dec(buff[0] & 0x7F);
-	alarm->minutes = BCD_to_dec(buff[1] & 0x7F);
-	alarm->hours = BCD_to_dec(buff[2] & 0x3F);
-	alarm->day_date = BCD_to_dec(buff[3] & 0x3F);
+    return res;
 }
 
-void ds3231_set_alarm_2(DS3231 *clock, DS3231_Alarm_2 *alarm)
+int8_t ds3231_get_alarm_2(ds3231_dev *clock, ds3231_alarm_2 *alarm)
 {
-	clock_store = clock;
+    uint8_t buff[3];
 
-	uint8_t buff[3];
+    alarm->alarm_rate = 0;
 
-	uint8_t a2m2 = (alarm->alarm_rate & 1) << 7;
-	uint8_t a2m3 = ((alarm->alarm_rate >> 1) & 1) << 7;
-	uint8_t a2m4 = ((alarm->alarm_rate >> 2) & 1) << 7;
-	uint8_t dy_dt = ((alarm->alarm_rate >> 3) & 1) << 6;
+    int8_t res = clock->read(DS3231_ALARM2_REG_ADDRESS, (uint8_t *)&buff, 3, clock->i2c_chip_ptr);
 
-	buff[0] = dec_to_BCD(alarm->minutes) | a2m2;
-	buff[1] = dec_to_BCD(alarm->hours) | a2m3;
-	buff[2] = dec_to_BCD(alarm->day_date) | a2m4 | dy_dt;
+    uint8_t a2m2 = (buff[0] >> 7) & 1;
+    uint8_t a2m3 = ((buff[1] >> 7) & 1) << 1;
+    uint8_t a2m4 = ((buff[2] >> 7) & 1) << 2;
+    uint8_t dy_dt = ((buff[2] >> 6) & 1) << 4;
 
-	write_data(0x0B, (uint8_t *)&buff, sizeof(buff));
+    alarm->alarm_rate = a2m2 | a2m3 | a2m4 | dy_dt;
+
+    alarm->minutes = BCD_to_dec(buff[0] & 0x7F);
+    alarm->hours = BCD_to_dec(buff[1] & 0x3F);
+    alarm->day_date = BCD_to_dec(buff[2] & 0x3F);
+
+    return res;
 }
 
-void ds3231_get_alarm_2(DS3231 *clock, DS3231_Alarm_2 *alarm)
+bool ds3231_check_alarm_interrupt(ds3231_dev *clock, ds3231_alarm alarm)
 {
-	uint8_t buff[3];
-
-	alarm->alarm_rate = 0;
-
-	read_data(ALARM2_REG_ADDRESS, (uint8_t *)&buff, 3);
-
-	uint8_t a2m2 = (buff[0] >> 7) & 1;
-	uint8_t a2m3 = ((buff[1] >> 7) & 1) << 1;
-	uint8_t a2m4 = ((buff[2] >> 7) & 1) << 2;
-	uint8_t dy_dt = ((buff[2] >> 6) & 1) << 4;
-
-	alarm->alarm_rate = a2m2 | a2m3 | a2m4 | dy_dt;
-
-	alarm->minutes = BCD_to_dec(buff[0] & 0x7F);
-	alarm->hours = BCD_to_dec(buff[1] & 0x3F);
-	alarm->day_date = BCD_to_dec(buff[2] & 0x3F);
+    uint8_t data = read_control_reg(clock, CONTROL_REG_1);
+    if (alarm == ALARM_1)
+        return (data >> DS3231_A1IE_A1F_BIT_NO) & 1;
+    else
+        return (data >> DS3231_A2IE_A2F_BIT_NO) & 1;
 }
 
-float ds3231_get_temperature(DS3231 *clock)
+int8_t ds3231_set_alarm_interrupt(ds3231_dev *clock, ds3231_alarm alarm, bool enable)
 {
-	uint8_t buff[2];
+    uint8_t data = read_control_reg(clock, CONTROL_REG_1);
+    uint8_t offset = alarm == ALARM_1 ? DS3231_A1IE_A1F_BIT_NO : DS3231_A2IE_A2F_BIT_NO;
 
-	read_data(TEMP_REG_ADDR, (uint8_t *)&buff, 2);
-	return (float)buff[0] + ((buff[1] >> 6) * 0.25f);
+    data &= ~(1 << offset);   // clear config
+    data |= enable << offset; // set enable
+
+    return write_control_reg(clock, CONTROL_REG_1, data);
+}
+
+bool ds3231_check_alarm_fired(ds3231_dev *clock, ds3231_alarm alarm)
+{
+    uint8_t data = read_control_reg(clock, CONTROL_REG_2);
+    if (alarm == ALARM_1)
+        return (data >> DS3231_A1IE_A1F_BIT_NO) & 1;
+    else
+        return (data >> DS3231_A2IE_A2F_BIT_NO) & 1;
+}
+
+uint8_t ds3231_reset_alarm_fired(ds3231_dev *clock, ds3231_alarm alarm)
+{
+    uint8_t data = read_control_reg(clock, CONTROL_REG_2);
+    uint8_t offset = alarm == ALARM_1 ? DS3231_A1IE_A1F_BIT_NO : DS3231_A2IE_A2F_BIT_NO;
+
+    data &= ~(1 << offset); // set 0 to alarm fired bit
+
+    return write_control_reg(clock, CONTROL_REG_2, data);
+}
+
+int8_t ds3231_set_intcn_mode(ds3231_dev *clock, ds3231_intcn intcn_value)
+{
+    uint8_t data = read_control_reg(clock, CONTROL_REG_1);
+    data &= ~(1 << DS3231_INTCN_BIT_NO);        // clear config
+    data |= intcn_value << DS3231_INTCN_BIT_NO; // set enable
+
+    return write_control_reg(clock, CONTROL_REG_1, data);
+}
+
+float ds3231_get_temperature(ds3231_dev *clock)
+{
+    uint8_t buff[2];
+
+    int8_t res = clock->read(DS3231_TEMP_REG_ADDR, (uint8_t *)&buff, 2, clock->i2c_chip_ptr);
+    return (float)buff[0] + ((buff[1] >> 6) * 0.25f);
 }
--- ds3231.h
+++ ds3231.h
@@ -1,75 +1,277 @@
 #pragma once
 
-#include "stm32f1xx.h"
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-	uint8_t address;			// chip address
-	I2C_HandleTypeDef *i2c_bus; // pointer to i2c handler
-} DS3231;
+#endif
 
-typedef struct
-{
-	uint8_t seconds;	 // 0-59
-	uint8_t minutes;	 // 0-59
-	uint8_t hours;		 // 0-23 (24 hour); 1-12 (12 hour)
-	uint8_t day_of_week; // 1-7
-	uint8_t date;		 // 1-31
-	uint8_t month;		 // 1-12
-	uint16_t year;		 //  2000-2099
-} DS3231_DateTime;
+#define DS3231_CLOCK_REG_ADDR 0x00 // start address of registers
+#define DS3231_ALARM1_REG_ADDRESS 0x07
+#define DS3231_ALARM2_REG_ADDRESS 0x0B
+#define DS3231_TEMP_REG_ADDR 0x11
 
-typedef enum
-{
-	A1_ONCE_PER_SECOND = 0x0F,						  // once per second
-	A1_SECONDS_MATCH = 0x0E,						  // when seconds match
-	A1_MINUTES_SECONDS_MATCH = 0x0C,				  // when minutes and seconds match
-	A1_HOURS_MINUTES_SECONDS_MATCH = 0x08,			  // when hours, minutes and seconds match
-	A1_DATE_HOURS_MINUTES_SECONDS_MATCH = 0x00,		  // when date, hours, minutes and seconds match
-	A1_DAY_OF_WEEK_HOURS_MINUTES_SECONDS_MATCH = 0x10 // when day of week, hours, minutes and seconds match
-} DS3231_Alarm_1_Alarm_Rate;
+#define DS3231_INTCN_BIT_NO 2    // index of INTCN bit right to left
+#define DS3231_A2IE_A2F_BIT_NO 1 // index of A2IE bit right to left
+#define DS3231_A1IE_A1F_BIT_NO 0 // index of A1IE bit right to left
 
-typedef struct
-{
-	uint8_t seconds;
-	uint8_t minutes;
-	uint8_t hours;
-	uint8_t day_date; // Range 1-7 // Range 0-31
-	DS3231_Alarm_1_Alarm_Rate alarm_rate;
-} DS3231_Alarm_1;
+#define DS3231_I2C_MASTER_TIMEOUT_MS 1000
 
-typedef enum
-{
-	A2_ONCE_PER_MINUTE = 0x07,				  // once per minute
-	A2_MINUTES_MATCH = 0x06,				  // when minutes match
-	A2_HOURS_MINUTES_MATCH = 0x04,			  // when hours and seconds match
-	A2_DATE_HOURS_MINUTES_MATCH = 0x00,		  // when date, hours and minutes match
-	A2_DAY_OF_WEEK_HOURS_MINUTES_MATCH = 0x08 // when day of week, hours and minutes match
-} DS3231_Alarm_2_Alarm_Rate;
+    /*!
+     * @brief Bus communication function pointer which should be mapped to
+     * the platform specific read functions of the user
+     *
+     * @param[in] reg_addr       : Register address from which data is read.
+     * @param[out] reg_data     : Pointer to data buffer where read data is stored.
+     * @param[in] len            : Number of bytes of data to be read.
+     * @param[in, out] intf_ptr  : Void pointer that can enable the linking of descriptors
+     *                                  for interface related call backs.
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     *
+     */
+    typedef int8_t (*ds3231_read_fptr_t)(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
 
-typedef struct
-{
-	uint8_t minutes;
-	uint8_t hours;
-	uint8_t day_date; // Range 1-7 // Range 0-31
-	DS3231_Alarm_2_Alarm_Rate alarm_rate;
-} DS3231_Alarm_2;
+    /*!
+     * @brief Bus communication function pointer which should be mapped to
+     * the platform specific write functions of the user
+     *
+     * @param[in] reg_addr      : Register address to which the data is written.
+     * @param[in] reg_data     : Pointer to data buffer in which data to be written
+     *                            is stored.
+     * @param[in] len           : Number of bytes of data to be written.
+     * @param[in, out] intf_ptr : Void pointer that can enable the linking of descriptors
+     *                            for interface related call backs
+     *
+     * @retval   0   -> Success.
+     * @retval Non zero value -> Fail.
+     *
+     */
+    typedef int8_t (*ds3231_write_fptr_t)(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len,
+                                          void *intf_ptr);
 
-DS3231 ds3231_init(uint8_t address, I2C_HandleTypeDef *i2c_bus);
+    /*!
+     * @brief ds3231 device
+     */
+    typedef struct
+    {
+        /* Interface function pointer used to enable the device address for I2C */
+        void *i2c_chip_ptr;
+        /* user defined read I2C funtion pointer */
+        ds3231_read_fptr_t read;
+        /* user defined write I2C funtion pointer */
+        ds3231_write_fptr_t write;
+    } ds3231_dev;
 
-void ds3231_set_date_time(DS3231 *clock, DS3231_DateTime *date_time);
-void ds3231_get_date_time(DS3231 *clock, DS3231_DateTime *date_time);
+    /*!
+     * @brief ds3231 date time store
+     */
+    typedef struct
+    {
+        uint8_t seconds;     // 0-59
+        uint8_t minutes;     // 0-59
+        uint8_t hours;       // 0-23 (24 hour)
+        uint8_t day_of_week; // 1-7
+        uint8_t date;        // 1-31
+        uint8_t month;       // 1-12
+        uint16_t year;       // 2000-2099
+    } ds3231_date_time;
 
-void ds3231_set_alarm_1(DS3231 *clock, DS3231_Alarm_1 *alarm);
-void ds3231_get_alarm_1(DS3231 *clock, DS3231_Alarm_1 *alarm);
+    /*!
+     * @brief ds3231 alarm 1 mode enum
+     */
+    typedef enum
+    {
+        A1_ONCE_PER_SECOND = 0x0F,                        // once per second
+        A1_SECONDS_MATCH = 0x0E,                          // when seconds match
+        A1_MINUTES_SECONDS_MATCH = 0x0C,                  // when minutes and seconds match
+        A1_HOURS_MINUTES_SECONDS_MATCH = 0x08,            // when hours, minutes and seconds match
+        A1_DATE_HOURS_MINUTES_SECONDS_MATCH = 0x00,       // when date, hours, minutes and seconds match
+        A1_DAY_OF_WEEK_HOURS_MINUTES_SECONDS_MATCH = 0x10 // when day of week, hours, minutes and seconds match
+    } ds3231_alarm_1_alarm_rate;
 
-void ds3231_enable_alarm_1(DS3231 *clock);
-void ds3231_disable_alarm_1(DS3231 *clock);
+    /*!
+     * @brief ds3231 alarm 1 configuration
+     */
+    typedef struct
+    {
+        uint8_t seconds;
+        uint8_t minutes;
+        uint8_t hours;
+        uint8_t day_date; // Range 1-7 // Range 0-31
+        ds3231_alarm_1_alarm_rate alarm_rate;
+    } ds3231_alarm_1;
 
-void ds3231_set_alarm_2(DS3231 *clock, DS3231_Alarm_2 *alarm);
-void ds3231_get_alarm_2(DS3231 *clock, DS3231_Alarm_2 *alarm);
+    /*!
+     * @brief ds3231 alarm 2 mode enum
+     */
+    typedef enum
+    {
+        A2_ONCE_PER_MINUTE = 0x07,                // once per minute
+        A2_MINUTES_MATCH = 0x06,                  // when minutes match
+        A2_HOURS_MINUTES_MATCH = 0x04,            // when hours and seconds match
+        A2_DATE_HOURS_MINUTES_MATCH = 0x00,       // when date, hours and minutes match
+        A2_DAY_OF_WEEK_HOURS_MINUTES_MATCH = 0x08 // when day of week, hours and minutes match
+    } ds3231_alarm_2_alarm_rate;
 
-void ds3231_enable_alarm_2(DS3231 *clock);
-void ds3231_disable_alarm_2(DS3231 *clock);
+    /*!
+     * @brief ds3231 alarm selector
+     */
+    typedef enum
+    {
+        ALARM_1 = 0x00,
+        ALARM_2 = 0x01
+    } ds3231_alarm;
 
-float ds3231_get_temperature(DS3231 *clock);
+    /*!
+     * @brief ds3231 INTCN selector
+     */
+    typedef enum
+    {
+        INTCN_SQUARE_WAVE = 0x00,
+        INTCN_ALARM_INTERRUPT = 0x01
+    } ds3231_intcn;
+
+    /*!
+     * @brief ds3231 control registers adrresses
+     */
+    typedef enum
+    {
+        CONTROL_REG_1 = 0x0E,
+        CONTROL_REG_2 = 0x0F
+    } ds3231_control_reg;
+
+    /*!
+     * @brief ds3231 alarm 2 configuration
+     */
+    typedef struct
+    {
+        uint8_t minutes;
+        uint8_t hours;
+        uint8_t day_date; // Range 1-7 // Range 0-31
+        ds3231_alarm_2_alarm_rate alarm_rate;
+    } ds3231_alarm_2;
+
+    /*!
+     * @brief Setup datetime
+     *
+     * @param[in] clock       Clock device
+     * @param[in] date_time   Filled date_time struct ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_set_date_time(ds3231_dev *clock, ds3231_date_time *date_time);
+    /*!
+     * @brief Get datetime from chip
+     *
+     * @param[in] clock        Clock device
+     * @param[out] date_time   Date_time ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_get_date_time(ds3231_dev *clock, ds3231_date_time *date_time);
+    /*!
+     * @brief Setup first alarm
+     *
+     * @param[in] clock       Clock device
+     * @param[in] date_time   Filled ds3231_alarm_1 struct ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_set_alarm_1(ds3231_dev *clock, ds3231_alarm_1 *alarm);
+    /*!
+     * @brief Get first alarm settings from chip
+     *
+     * @param[in] clock       Clock device
+     * @param[out] alarm      ds3231_alarm_1 date_time ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_get_alarm_1(ds3231_dev *clock, ds3231_alarm_1 *alarm);
+    /*!
+     * @brief Setup second alarm
+     *
+     * @param[in] clock       Clock device
+     * @param[in] date_time   Filled ds3231_alarm_2 struct ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_set_alarm_2(ds3231_dev *clock, ds3231_alarm_2 *alarm);
+    /*!
+     * @brief Get second alarm settings from chip
+     *
+     * @param[in] clock       Clock device
+     * @param[out] alarm      ds3231_alarm_2 date_time ptr
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_get_alarm_2(ds3231_dev *clock, ds3231_alarm_2 *alarm);
+    /*!
+     * @brief Get alarm flag status
+     *
+     * @param[in] clock       Clock device
+     * @param[in] alarm       Alarm selector
+     *
+     * @retval   Alarm flag status
+     */
+    bool ds3231_check_alarm_interrupt(ds3231_dev *clock, ds3231_alarm alarm);
+    /*!
+     * @brief Get alarm fired flag status
+     *
+     * @param[in] clock       Clock device
+     * @param[in] alarm       Alarm selector
+     *
+     * @retval   Alarm fired flag status
+     */
+    bool ds3231_check_alarm_fired(ds3231_dev *clock, ds3231_alarm alarm);
+    /*!
+     * @brief Reset fired flag. Use to restart alarm with existed settings
+     *
+     * @param[in] clock       Clock device
+     * @param[in] alarm       Alarm selector
+     *
+     * @retval   Alarm fired flag status
+     */
+    uint8_t ds3231_reset_alarm_fired(ds3231_dev *clock, ds3231_alarm alarm);
+    /*!
+     * @brief Enable or disable alarm interrupt
+     *
+     * @param[in] clock       Clock device
+     * @param[in] alarm       Alarm selector
+     * @param[in] enable      Enable/disable
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_set_alarm_interrupt(ds3231_dev *clock, ds3231_alarm alarm, bool enable);
+    /*!
+     * @brief Change INTCN flag status
+     *
+     * @param[in] clock       Clock device
+     * @param[in] intcn_value New Value
+     *
+     * @retval   0 -> Success.
+     * @retval Non zero value -> Fail.
+     */
+    int8_t ds3231_set_intcn_mode(ds3231_dev *clock, ds3231_intcn intcn_value);
+    /*!
+     * @brief Get value from chip temperature sensor
+     *
+     * @param[in] clock       Clock device
+     *
+     * @retval   Temperature in ℃
+     */
+    float ds3231_get_temperature(ds3231_dev *clock);
+
+#ifdef __cplusplus
+}
+#endif /* End of CPP guard */
